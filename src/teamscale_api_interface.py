dd66b7c16751a1e8cef5e81c6ec57754d8721938
schuhmaj <jonas.schuhmacher@tum.de>
2022-06-03 15:56:33 +0200

    created post_project_git

--- src/teamscale_api_interface.py
+++ src/teamscale_api_interface.py
@@ -24,7 +24,7 @@ def get_project(project_id: str = None):
 
     """
     if project_id is None:
-        response = requests.get(TEAMSCALE_REST_URL + f"projects/", auth=TEAMSCALE_AUTHENTICATION)
+        response = requests.get(TEAMSCALE_REST_URL + f"projects", auth=TEAMSCALE_AUTHENTICATION)
         return json.loads(response.text, object_hook=lambda d: SimpleNamespace(**d))
     else:
         response = requests.get(TEAMSCALE_REST_URL + f"projects/{project_id}",
@@ -32,11 +32,92 @@ def get_project(project_id: str = None):
         return json.loads(response.text, object_hook=lambda d: SimpleNamespace(**d))
 
 
-def post_project(project_id: str):
-    pass
+def get_project_configuration(project_id: str):
+    """
+    Gets the full project configuration for a certain project given a project_id.
+
+    Args:
+        project_id: a project id
+
+    Returns:
+        the project's configuration
+
+    """
+    response = requests.get(TEAMSCALE_REST_URL + f"projects/{project_id}/configuration",
+                            auth=TEAMSCALE_AUTHENTICATION)
+    print(response.text)
+    return json.loads(response.text, object_hook=lambda d: SimpleNamespace(**d))
 
 
-def get_findings(project_id: str, path: str = '', filter_findings: [str] = None, invert: bool = True, truncate: bool = True):
+def post_project_git(project_id: str, project_git_repo: str, default_branch: str):
+    """
+    Creates a new project in Teamscale with the given id, git-repo path and default_branch name. The git-account is
+    hardcoded to "schuhmaj" and should have been previously created in Teamscale!
+
+    Args:
+        project_id: the project id for referencing the project via the API (also the project name)
+        project_git_repo: the path on GitHub, e.g. schuhmaj/nasa-breakup-model-cpp
+        default_branch: the default branch name, e.g. main
+
+    Returns:
+        True if the project was successfully created
+
+    """
+    response = requests.post(TEAMSCALE_REST_URL + f"projects", auth=TEAMSCALE_AUTHENTICATION,
+                             json={
+                                 "name": f"{project_id}",
+                                 "publicIds": [
+                                     f"{project_id}"
+                                 ],
+                                 "profile": "C/C++ (default)",
+                                 "connectors": [
+                                     {
+                                         "type": "Git",
+                                         "connectorIdentifierOptionName": "Repository identifier",
+                                         "options": {
+                                             "Account": "schuhmaj",
+                                             "Path suffix": f"{project_git_repo}",
+                                             "Repository identifier": f"{project_id}-repo",
+                                             "Included file names": "**.cpp, **.cc, **.c, **.h, **.hh, **.hpp, **.cxx, **.hxx, **.inl, **.inc, **.architecture",
+                                             "Excluded file names": "",
+                                             "Important Branches": "",
+                                             "Include Submodules": "false",
+                                             "Submodule recursion depth": "10",
+                                             "SSH Private Key ID": "",
+                                             "Default branch name": f"{default_branch}",
+                                             "Enable branch analysis": "false",
+                                             "Included branches": ".*",
+                                             "Excluded branches": "_anon.*",
+                                             "Start revision": "1 year ago",
+                                             "Content exclude": "",
+                                             "Polling interval": "60",
+                                             "Test-code path pattern": "",
+                                             "Test-code path exclude pattern": "",
+                                             "Prepend repository identifier": "false",
+                                             "End revision": "",
+                                             "Text filter": "",
+                                             "Language mapping": "",
+                                             "Analysis report mapping": "**/compile_commands.json -> COMPILATION_DATABASE",
+                                             "Partition Pattern": "",
+                                             "File-size exclude": "1MB",
+                                             "Source library connector": "false",
+                                             "Run to exhaustion": "false",
+                                             "Preserve empty commits": "true",
+                                             "Delta size": "500",
+                                             "Path prefix transformation": "",
+                                             "Path transformation": "",
+                                             "Encoding": "",
+                                             "Author transformation": "",
+                                             "Branch transformation": ""
+                                         }
+                                     }
+                                 ]
+                             })
+    return response.status_code == 201
+
+
+def get_findings(project_id: str, path: str = '', filter_findings: [str] = None, invert: bool = True,
+                 truncate: bool = True):
     """
     Get the findings of a specific project given a project id.
 
@@ -69,9 +150,16 @@ def get_findings(project_id: str, path: str = '', filter_findings: [str] = None,
     return objects
 
 
-m = get_findings("breakup-model-cpp", filter_findings="Redundancy")
-print(len(m))
+# m = get_findings("breakup-model-cpp", filter_findings="Redundancy")
+# print(len(m))
+#
+# breakup_project = get_project("breakup-model-cpp")
+#
+# breakup_project_config = get_project_configuration("breakup-model-cpp")
+#
+# print(breakup_project_config)
+#
+# print(breakup_project)
 
-breakup_project = get_project()
+res = post_project_git("polyhedral-gravity-model-cpp", "schuhmaj/polyhedral-gravity-model-cpp", "main")
 
-print(breakup_project)
