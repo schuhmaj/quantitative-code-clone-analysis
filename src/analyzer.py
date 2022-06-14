d0f9bf196ed5f004119dbc61a7da881b7bbc3ec6
schuhmaj <jonas.schuhmacher@tum.de>
2022-06-14 20:29:14 +0200

    added check for access token

--- src/analyzer.py
+++
@@ -1,7 +0,0 @@
-from tqdm import tqdm
-from model.project import Project, load
-
-if __name__ == "__main__":
-    cpp_projects = load("../model_output/cpp_projects.pickle")
-    for project in cpp_projects:
-        print(project)
