cb13d2edeb0d80eaaf63d110daeea446c49a3b62
schuhmaj <jonas.schuhmacher@tum.de>
2022-06-16 23:06:58 +0200

    added some utiliyt methods

--- src/model/project.py
+++ src/model/project.py
@@ -19,7 +19,7 @@ class Project:
         return f"Project ID: {self.project_id}, Repo Full Name: {self.repo_full_name}, Language: {self.language}," \
                f"Default Branch: {self.branch}, Revision: {self.revision}, Metrics: {str(self.metrics)}"
 
-    def get_method_length_assessment(self):
+    def get_method_length_assessment(self) -> (float, float, float):
         """
         Returns the method length assessment (order Red, Yellow, Green) percentages.
 
@@ -30,7 +30,7 @@ class Project:
         text = self.metrics["overview"][5]["formattedTextValue"]
         return tuple([float(x) for x in re.findall(r"\w: \d+ \((\d+.\d+)%\)", text)])
 
-    def has_metrics(self):
+    def has_metrics(self) -> bool:
         """
         Returns False if some metrics are None
         Returns:
@@ -41,6 +41,24 @@ class Project:
             if value is None:
                 return False
         return True
+
+    def get_clone_coverage(self) -> float:
+        """
+        Returns the clone coverage as percentage value
+        Returns:
+            clone coverage
+
+        """
+        return self.metrics["overview"][10]["value"] * 100.0
+
+    def get_source_lines_of_code(self) -> int:
+        """
+        Returns the source lines of code
+        Returns:
+            source lines of code
+
+        """
+        return self.metrics["overview"][2]["value"] * 100.0
 
 
 def save(filepath: str, projects: [Project]):
